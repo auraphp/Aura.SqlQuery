10eb0b1ab8d05063eb3759faa23ba9ee24d67621
Boris Momčilović <boris.momcilovic@gmail.com>
2014-10-29 13:02:35 +0100

    MySQL Insert: ON DUPLICATE KEY UPDATE functionality

--- src/Mysql/Insert.php
+++ src/Mysql/Insert.php
@@ -23,6 +23,16 @@ class Insert extends Common\Insert
 {
     /**
      *
+     * Column values for ON DUPLICATE KEY UPDATE section of query; the key is
+     * the column name and the value is the column value.
+     *
+     * @param array
+     *
+     */
+    protected $col_on_update_values;
+
+    /**
+     *
      * Adds or removes HIGH_PRIORITY flag.
      *
      * @param bool $enable Set or unset flag (default true).
@@ -79,5 +89,121 @@ class Insert extends Common\Insert
     {
         $this->setFlag('DELAYED', $enable);
         return $this;
+    }
+
+    /**
+     *
+     * Sets one column value placeholder in ON DUPLICATE KEY UPDATE section;
+     * if an optional second parameter is passed, that value is bound to the
+     * placeholder.
+     *
+     * @param string $col The column name.
+     *
+     * @param mixed $val Optional: a value to bind to the placeholder.
+     *
+     * @return self
+     *
+     */
+    public function colOnUpdate($col)
+    {
+        $key = $this->quoter->quoteName($col);
+        $bind = 'on_update_' . $col;
+        $this->col_on_update_values[$key] = ":$bind";
+        $args = func_get_args();
+        if (count($args) > 1) {
+            $this->bindValue($bind, $args[1]);
+        }
+        return $this;
+    }
+
+    /**
+     *
+     * Sets multiple column value placeholders in ON DUPLICATE KEY UPDATE
+     * section. If an element is a key-value pair, the key is treated as the
+     * column name and the value is bound to that column.
+     *
+     * @param array $cols A list of column names, optionally as key-value
+     * pairs where the key is a column name and the value is a bind value for
+     * that column.
+     *
+     * @return self
+     *
+     */
+    public function colsOnUpdate(array $cols)
+    {
+        foreach ($cols as $key => $val) {
+            if (is_int($key)) {
+                // integer key means the value is the column name
+                $this->colOnUpdate($val);
+            } else {
+                // the key is the column name and the value is a value to
+                // be bound to that column
+                $this->colOnUpdate($key, $val);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     *
+     * Sets a column value directly in ON DUPLICATE KEY UPDATE section; the
+     * value will not be escaped, although fully-qualified identifiers in the
+     * value will be quoted.
+     *
+     * @param string $col The column name.
+     *
+     * @param string $value The column value expression.
+     *
+     * @return self
+     *
+     */
+    public function setOnUpdate($col, $value)
+    {
+        if ($value === null) {
+            $value = 'NULL';
+        }
+
+        $key = $this->quoter->quoteName($col);
+        $value = $this->quoter->quoteNamesIn($value);
+        $this->col_on_update_values[$key] = $value;
+        return $this;
+    }
+
+    /**
+     *
+     * Builds this query object into a string.
+     *
+     * @return string
+     *
+     */
+    protected function build()
+    {
+        return 'INSERT'
+            . $this->buildFlags()
+            . $this->buildInto()
+            . $this->buildValuesForInsert()
+            . $this->buildValuesForUpdateOnDuplicateKey()
+            . $this->buildReturning();
+    }
+
+    /**
+     *
+     * Builds the UPDATE ON DUPLICATE KEY part of the statement.
+     *
+     * @return string
+     *
+     */
+    protected function buildValuesForUpdateOnDuplicateKey()
+    {
+        if (! $this->col_on_update_values) {
+            return ''; // not applicable
+        }
+
+        foreach ($this->col_on_update_values as $key => $row) {
+            $values[] = $this->indent([$key . ' = ' . $row]);
+        }
+
+        return ' ON DUPLICATE KEY UPDATE'
+            . implode (',', $values);
     }
 }
--- tests/unit/src/Mysql/InsertTest.php
+++ tests/unit/src/Mysql/InsertTest.php
@@ -23,6 +23,23 @@ class InsertTest extends Common\InsertTest
         )
     ";
 
+    protected $expected_sql_on_duplicate_key_update = "
+        INSERT INTO <<t1>> (
+            <<c1>>,
+            <<c2>>,
+            <<c3>>,
+            <<c4>>,
+            <<c5>>
+        ) VALUES (
+            :c1,
+            :c2,
+            :c3,
+            NOW(),
+            NULL
+        ) ON DUPLICATE KEY UPDATE
+            <<c4>> = %s
+    ";
+
     public function testHighPriority()
     {
         $this->query->highPriority()
@@ -75,6 +92,48 @@ class InsertTest extends Common\InsertTest
 
         $actual = $this->query->__toString();
         $expect = sprintf($this->expected_sql_with_flag, 'IGNORE');
+
+        $this->assertSameSql($expect, $actual);
+    }
+
+	public function testColOnUpdate ()
+    {
+        $this->query->colOnUpdate ('c4')
+                    ->into('t1')
+                    ->cols(array('c1', 'c2', 'c3'))
+                    ->set('c4', 'NOW()')
+                    ->set('c5', null);
+
+        $actual = $this->query->__toString();
+        $expect = sprintf ($this->expected_sql_on_duplicate_key_update, ':on_update_c4');
+
+        $this->assertSameSql($expect, $actual);
+    }
+
+	public function testColsOnUpdate ()
+    {
+        $this->query->colsOnUpdate (['c4' => null])
+                    ->into('t1')
+                    ->cols(array('c1', 'c2', 'c3'))
+                    ->set('c4', 'NOW()')
+                    ->set('c5', null);
+
+        $actual = $this->query->__toString();
+        $expect = sprintf ($this->expected_sql_on_duplicate_key_update, ':on_update_c4');
+
+        $this->assertSameSql($expect, $actual);
+    }
+
+    public function testSetOnUpdate()
+    {
+        $this->query->setOnUpdate ('c4', 'NOW()')
+                    ->into('t1')
+                    ->cols(array('c1', 'c2', 'c3'))
+                    ->set('c4', 'NOW()')
+                    ->set('c5', null);
+
+        $actual = $this->query->__toString();
+        $expect = sprintf ($this->expected_sql_on_duplicate_key_update, 'NOW()');
 
         $this->assertSameSql($expect, $actual);
     }
