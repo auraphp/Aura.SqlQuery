12bade5484f0300314b88067430c0d51308b84f7
Paul M. Jones <pmjones88@gmail.com>
2015-03-06 08:30:54 -0600

    use the os-specific line ending

--- tests/unit/src/AbstractQueryTest.php
+++ tests/unit/src/AbstractQueryTest.php
@@ -40,9 +40,9 @@ abstract class AbstractQueryTest extends \PHPUnit_Framework_TestCase
         $actual = preg_replace('/^[ \t]*/m', '', $actual);
         $actual = preg_replace('/[ \t]*$/m', '', $actual);
 
-        // remove all line endings to be sure tests will pass on windows and mac
-        $expect = preg_replace('/\r\n|\n|\r/', ' ', $expect);
-        $actual = preg_replace('/\r\n|\n|\r/', ' ', $actual);
+        // normalize line endings to be sure tests will pass on windows and mac
+        $expect = preg_replace('/\r\n|\n|\r/', PHP_EOL, $expect);
+        $actual = preg_replace('/\r\n|\n|\r/', PHP_EOL, $actual);
 
         // are they the same now?
         $this->assertSame($expect, $actual);
